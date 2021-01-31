f5c4f15d5291cea3c945134abfebcb79ab03b7cd
gians <giansluca@gmail.com>
2021-01-31 18:11:38 +0100

    update env

--- env-production.sh
+++ env-production.sh
@@ -3,6 +3,6 @@
 # ENV
 export NODE_ENV=production
 # DB
-export DB_URI=mongodb+srv://vidly-user:12345@cluster0-tjxbs.mongodb.net/vidly
+export DB_URI=
 # JWT 
-export VIDLY_JWT_PRIVATE_KEY=verySecret-verySecret
+export VIDLY_JWT_PRIVATE_KEY=
